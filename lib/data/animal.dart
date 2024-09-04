4bc78e004fbadcb7350bb6907ae5791f4cbe7247
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-03 21:41:56 -0500

    fix: adding animal fields

--- lib/data/animal.dart
+++ lib/data/animal.dart
@@ -1,7 +1,35 @@
 import 'resource.dart';
 
+enum Sex { male, female }
+
 class Animal extends Resource {
-  DateTime birthDate;
-  bool isFixed; // In relation to neutering/castrasting/spaying
+  late DateTime birthDate;
+  bool isFixed = false; // In relation to neutering/castrasting/spaying
   late DateTime deathDate;
+  String nickname = "";
+  late Sex sex;
+
+  Animal(super.id, super.created, super.modified, super.tags, this.birthDate,
+      this.isFixed, this.deathDate, this.nickname, this.sex);
+
+  ///
+  Animal.fromMap(Map<String, dynamic> data) : super.fromMap(data) {
+    birthDate = data["birthDate"];
+    isFixed = data["isFixed"];
+    nickname = data["nickname"];
+    sex = data["sex"];
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      "isFixed": isFixed,
+      "birthDate": birthDate,
+      "deathDate": deathDate,
+      "nickname": nickname,
+      "sex": sex
+    };
+    map.addAll(super.toMap());
+    return map;
+  }
 }
