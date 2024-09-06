c149bbdfae7f21b16d1a0192316db1929bbcc4d2
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-05 21:21:18 -0500

    fix: adding single pages export

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'pages/home.dart';
+import 'pages/pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
