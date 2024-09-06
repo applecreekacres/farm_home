c7a0ee3295d67897c2fe3466d88062228041defe
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-05 22:03:48 -0500

    feat: adding farmer class

---
+++ lib/data/farmer.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:farm_home/constants/constants.dart';
+
+class Farmer {
+  final String id;
+  final String photoUrl;
+  final String name;
+  final String description;
+
+  const Farmer(
+      {required this.id,
+      required this.photoUrl,
+      required this.name,
+      required this.description});
+
+  Map<String, String> toJson() {
+    return {
+      FirestoreConstants.name: name,
+      FirestoreConstants.description: description,
+      FirestoreConstants.photoUrl: photoUrl,
+    };
+  }
+
+  factory Farmer.fromDocument(DocumentSnapshot doc) {
+    String description = "";
+    String photoUrl = "";
+    String name = "";
+    try {
+      description = doc.get(FirestoreConstants.description);
+    } catch (_) {}
+    try {
+      photoUrl = doc.get(FirestoreConstants.photoUrl);
+    } catch (_) {}
+    try {
+      name = doc.get(FirestoreConstants.name);
+    } catch (_) {}
+    return Farmer(
+      id: doc.id,
+      photoUrl: photoUrl,
+      name: name,
+      description: description,
+    );
+  }
+}
