80511bddc3519799b595b087a66704f4d8568fdd
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-06 23:56:28 -0500

    chore: updating formatting

--- lib/models/potting_up_record.dart
+++ lib/models/potting_up_record.dart
@@ -1,5 +1,6 @@
 import 'record.dart';
 
 class PottingUpRecord extends Record {
-  PottingUpRecord(super.id, super.created, super.modified, super.tags, super.title, super.description, super.isDone, super.quantities);
+  PottingUpRecord(super.id, super.created, super.modified, super.tags,
+      super.title, super.description, super.isDone, super.quantities);
 }
