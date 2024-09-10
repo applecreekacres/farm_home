3f8e12454327492f4870b7e8b1bb2ba6e091a949
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-09 22:25:34 -0500

    test: adding more tests for record

--- test/models_test.dart
+++ test/models_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:farm_home/models/models.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   test("Create a new Record", () {
@@ -22,5 +23,39 @@ void main() {
     expect(record.tags.length, 1);
     expect(record.tags[0], "tag");
     expect(record.description, "Basic Description");
+    expect(record.recordType, "Record");
+  });
+
+  test("Load Record from Map", () {
+    final now = DateTime.now().millisecondsSinceEpoch;
+    final id = Uuid().v4();
+    Map<String, dynamic> map = {
+      "id": id,
+      "created":
+          DateTime(DateTime.now().year, 1, 2, 3, 4, 5).millisecondsSinceEpoch,
+      "modified":
+          DateTime(DateTime.now().year, 5, 4, 3, 2, 1).millisecondsSinceEpoch,
+      "title": "title",
+      "description": "description",
+      "isDone": false,
+      "timestamp": now,
+      "tags": ["tag2"],
+      "quantities": [],
+      "resources": [],
+      "recordType": (Record).toString()
+    };
+
+    final record = Record.fromMap(map);
+
+    expect(record.title, "title");
+  });
+
+  test("Get Map from Record", () {
+    final now = DateTime.now();
+    final record =
+        Record(["test"], "Record", now, "", true, [], [], (Record).toString());
+    final map = record.toMap();
+
+    expect(map["title"], "Record");
   });
 }
