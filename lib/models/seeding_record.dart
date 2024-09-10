e2d92a5add8106e407b3bf389b3d8f8f5dce282e
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-10 05:55:46 -0500

    feat(seeding): adding fields to record

--- lib/models/seeding_record.dart
+++ lib/models/seeding_record.dart
@@ -1,7 +1,28 @@
 import 'models.dart';
 
+enum SeedingMethod { direct, greenhouseSown, transplantFromPurchased }
+
 class SeedingRecord extends Record {
-  late DateTime seedingDate;
+  DateTime _seedingDate = DateTime.now();
+  SeedingMethod _seedingMethod = SeedingMethod.direct;
+
+  DateTime get seedingDate => _seedingDate;
+
+  SeedingMethod get seedingMethod => _seedingMethod;
+
+  set seedingDate(DateTime date) {
+    if (date != _seedingDate) {
+      _seedingDate = date;
+      update();
+    }
+  }
+
+  set seedingMethod(SeedingMethod method) {
+    if (method != _seedingMethod) {
+      _seedingMethod = method;
+      update();
+    }
+  }
 
   SeedingRecord(
       List<String> tags,
@@ -11,18 +32,21 @@ class SeedingRecord extends Record {
       bool isDone,
       List<Quantity> quantities,
       List<Resource> resources,
-      this.seedingDate)
+      this._seedingDate,
+      this._seedingMethod)
       : super(tags, title, timestamp, description, isDone, quantities,
             resources, (SeedingRecord).toString());
 
   SeedingRecord.fromMap(Map<String, dynamic> data) : super.fromMap(data) {
-    seedingDate = DateTime.fromMillisecondsSinceEpoch(data["seedingDate"]);
+    _seedingDate = DateTime.fromMillisecondsSinceEpoch(data["seedingDate"]);
+    _seedingMethod = data["seedingMethod"];
   }
 
   @override
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = {
-      "seedingDate": seedingDate.millisecondsSinceEpoch
+      "seedingDate": _seedingDate.millisecondsSinceEpoch,
+      "seedingMethod": _seedingMethod
     };
     map.addAll(super.toMap());
     return map;
