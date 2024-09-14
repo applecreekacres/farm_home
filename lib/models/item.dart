158aeab748dd06a952914d021b0cd79c4805fd85
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-13 21:31:01 -0500

    chore: documenting class

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uuid/uuid.dart';
 
+/// Based class for pretty much all items that will be used within the app.
+/// This contains the base fields that all classes need. An unique ID, a
+/// creation timestamp and a modified timestamp.
 class Item {
   String _id = const Uuid().v4();
   DateTime _created = DateTime.now();
@@ -9,15 +12,14 @@ class Item {
 
   Item(this.tags);
 
+  /// Unique identifier of every item and derived class that is created.
   String get id => _id;
 
+  /// Timestamp of when the item was created.
   DateTime get created => _created;
 
+  /// Timestamp of when the item was last modified.
   DateTime get modified => _modified;
-
-  void update() {
-    _modified = DateTime.now();
-  }
 
   Item.fromMap(Map<String, dynamic> data) {
     _id = data["id"];
@@ -36,8 +38,12 @@ class Item {
   }
 }
 
+/// Get an item from Firestore with a specific identification.
+///
+/// Will return [null] if no item by the given ID exists.
 Future<T?> getItemById<T extends Item>(String itemId) async {
-  final docRef = FirebaseFirestore.instance.collection((T).toString()).doc(itemId);
+  final docRef =
+      FirebaseFirestore.instance.collection((T).toString()).doc(itemId);
   final docSnapshot = await docRef.get();
   if (docSnapshot.exists) {
     return Item.fromMap(docSnapshot.data()!) as T;
@@ -46,6 +52,7 @@ Future<T?> getItemById<T extends Item>(String itemId) async {
   }
 }
 
+/// Upload an item to Firestore. If the item exists it will be updated.
 void updateItem<T extends Item>(T model) async {
   model._modified = DateTime.now();
   await FirebaseFirestore.instance
