234be494b9857278ea1039bd69f4da847823a5c5
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-13 20:58:31 -0500

    fix: proving out uploading season data

--- lib/pages/season_form.dart
+++ lib/pages/season_form.dart
@@ -1,5 +1,6 @@
 import 'package:farm_home/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:farm_home/widgets/widgets.dart';
 
 class SeasonFormPage extends StatefulWidget {
   const SeasonFormPage({super.key});
@@ -43,45 +44,57 @@ class _SeasonFormPageState extends State<SeasonFormPage> {
     }
   }
 
+  void _saveSeason(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("New Season"),
+          actions: [
+            IconButton(
+                onPressed: () => updateItem<Season>(_season),
+                icon: const Icon(Icons.save))
+          ],
         ),
+        drawer: const FarmHomeDrawer(),
         body: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Name"),
-                validator: (value) =>
-                    value?.isEmpty ?? true ? 'Please enter a name' : null,
-                onChanged: (value) {
-                  _season.name = value;
-                },
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "Name"),
+                    validator: (value) =>
+                        value?.isEmpty ?? true ? 'Please enter a name' : null,
+                    onChanged: (value) {
+                      _season.name = value;
+                    },
+                  ),
+                  TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                        labelText: 'Start Date',
+                        suffixIcon: IconButton(
+                            onPressed: () => _selectStartDate(context),
+                            icon: const Icon(Icons.calendar_today))),
+                    controller:
+                        TextEditingController(text: _season.start.toString()),
+                  ),
+                  TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                        labelText: 'End Date',
+                        suffixIcon: IconButton(
+                            onPressed: () => _selectEndDate(context),
+                            icon: const Icon(Icons.calendar_today))),
+                    controller:
+                        TextEditingController(text: _season.end.toString()),
+                  )
+                ],
               ),
-              TextFormField(
-                readOnly: true,
-                decoration: InputDecoration(
-                    labelText: 'Start Date',
-                    suffixIcon: IconButton(
-                        onPressed: () => _selectStartDate(context),
-                        icon: const Icon(Icons.calendar_today))),
-                controller:
-                    TextEditingController(text: _season.start.toString()),
-              ),
-              TextFormField(
-                readOnly: true,
-                decoration: InputDecoration(
-                    labelText: 'End Date',
-                    suffixIcon: IconButton(
-                        onPressed: () => _selectEndDate(context),
-                        icon: const Icon(Icons.calendar_today))),
-                controller: TextEditingController(text: _season.end.toString()),
-              )
-            ],
-          ),
-        ));
+            )));
   }
 }
