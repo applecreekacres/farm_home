4bd64d0e793229eb71b625b6811092835cb7c75b
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-14 21:41:23 -0500

    fix: simplifying class

--- lib/models/farmer.dart
+++ lib/models/farmer.dart
@@ -1,45 +1,20 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:farm_home/constants/constants.dart';
 import 'package:farm_home/models/models.dart';
 
 class Farmer extends Item {
-  final String id;
-  final String photoUrl;
-  final String name;
-  final String description;
+  String photoUrl = "";
+  String name = "";
 
-  const Farmer(
-      {required this.id,
-      required this.photoUrl,
-      required this.name,
-      required this.description});
+  Farmer(this.name, this.photoUrl);
 
-  Map<String, String> toJson() {
-    return {
-      FirestoreConstants.name: name,
-      FirestoreConstants.description: description,
-      FirestoreConstants.photoUrl: photoUrl,
-    };
+  Farmer.fromMap(Map<String, dynamic> data) : super.fromMap(data) {
+    name = data["name"];
+    photoUrl = data["photoUrl"];
   }
 
-  factory Farmer.fromDocument(DocumentSnapshot doc) {
-    String description = "";
-    String photoUrl = "";
-    String name = "";
-    try {
-      description = doc.get(FirestoreConstants.description);
-    } catch (_) {}
-    try {
-      photoUrl = doc.get(FirestoreConstants.photoUrl);
-    } catch (_) {}
-    try {
-      name = doc.get(FirestoreConstants.name);
-    } catch (_) {}
-    return Farmer(
-      id: doc.id,
-      photoUrl: photoUrl,
-      name: name,
-      description: description,
-    );
+  @override
+  Map<String, dynamic> toMap() {
+    var map = super.toMap();
+    map.addAll({"name": name, "photoUrl": photoUrl});
+    return map;
   }
 }
