7da76c90edc825e129d055d4b7a7afe39830a2d6
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-14 21:40:21 -0500

    feat: adding getting a list of items

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uuid/uuid.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// Based class for pretty much all items that will be used within the app.
 /// This contains the base fields that all classes need. An unique ID, a
 /// creation timestamp and a modified timestamp.
 class Item {
+  String _userId = "";
   String _id = const Uuid().v4();
   DateTime _created = DateTime.now();
   DateTime _modified = DateTime.now();
@@ -22,6 +24,7 @@ class Item {
 
   Item.fromMap(Map<String, dynamic> data) {
     _id = data["id"];
+    _userId = data["userid"];
     _created = DateTime.fromMillisecondsSinceEpoch(data["created"]);
     _modified = DateTime.fromMillisecondsSinceEpoch(data["modified"]);
   }
@@ -29,6 +32,7 @@ class Item {
   Map<String, dynamic> toMap() {
     return {
       "id": _id,
+      "userId": _userId,
       "created": _created.millisecondsSinceEpoch,
       "modified": _modified.millisecondsSinceEpoch
     };
@@ -49,8 +53,22 @@ Future<T?> getItemById<T extends Item>(String itemId) async {
   }
 }
 
+Future<List<T>> getItemsByUser<T extends Item>(String itemId) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var user = prefs.getString("userId");
+  final docRef = FirebaseFirestore.instance
+      .collection((T).toString())
+      .where('userId', isEqualTo: user);
+  final docSnapshot = await docRef.get();
+  final itemList =
+      docSnapshot.docs.map((doc) => Item.fromMap(doc.data()) as T).toList();
+  return itemList;
+}
+
 /// Upload an item to Firestore. If the item exists it will be updated.
 void updateItem<T extends Item>(T model) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  model._userId = prefs.getString("userId") ?? "";
   model._modified = DateTime.now();
   await FirebaseFirestore.instance
       .collection((T).toString())
