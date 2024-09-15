6ace801aac22a84df3b6f829463387b2b8c5849c
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-14 22:55:45 -0500

    feat(equipment): adding specifc fields

--- lib/models/equipment.dart
+++ lib/models/equipment.dart
@@ -1,5 +1,29 @@
 import 'resource.dart';
 
 class Equipment extends Resource {
-  Equipment() : super();
+  String manufacturer = "";
+  String model = "";
+  String serialNumber = "";
+
+  Equipment(this.manufacturer, this.model, this.serialNumber) : super();
+
+  Equipment.fromMap(Map<String, dynamic> data) : super.fromMap(data) {
+    if (data["resourceType"] == resourceType) {
+      manufacturer = data["manufacturer"];
+      model = data["model"];
+      serialNumber = data["serialNumber"];
+    } else {
+      ArgumentError("Data is not of correct resource type.");
+    }
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = super.toMap();
+    map.addAll({
+      "manufacturer": manufacturer,
+      "model": model,
+      "serialNumber": serialNumber
+    });
+    return map;
+  }
 }
