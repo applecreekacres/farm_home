48bcdd8a968a53d1fe8f637b150f41b8de376a4f
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-18 12:01:56 -0500

    fix(record_form): adding ability to customize fields

--- lib/widgets/record_form.dart
+++ lib/widgets/record_form.dart
@@ -5,8 +5,9 @@ import 'package:farm_home/models/models.dart';
 
 class RecordForm<T extends Record> extends StatefulWidget {
   final T record;
+  List<Widget>? additionalFields = List<Widget>.empty();
 
-  const RecordForm({required this.record, super.key});
+  RecordForm({required this.record, this.additionalFields, super.key});
 
   @override
   State<RecordForm> createState() => _RecordFormState<T>();
@@ -15,7 +16,30 @@ class RecordForm<T extends Record> extends StatefulWidget {
 class _RecordFormState<T extends Record> extends State<RecordForm<T>> {
   final _formKey = GlobalKey<FormState>();
   T get record => widget.record;
+  List<Widget> get fields => widget.additionalFields ?? List<Widget>.empty();
   String get recordName => record.recordType.replaceFirst("Record", '');
+
+  List<Widget> _buildFields() {
+    List<Widget> fields = [
+      TextFormField(
+          decoration: const InputDecoration(labelText: "Name"),
+          validator: (value) =>
+              value?.isEmpty ?? true ? 'Please enter a name' : null,
+          onChanged: (value) => record.title = value),
+      LabelledCheckbox(label: 'Done', value: record.isDone),
+      DateTimeTextField(
+          labelText: 'Timestamp',
+          value: record.timestamp,
+          onChanged: (value) {
+            record.timestamp = value!;
+          },
+          initialDate: DateTime.now()),
+      NotesField(modelField: record.notes),
+    ];
+
+    fields.addAll(this.fields);
+    return fields;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,23 +68,7 @@ class _RecordFormState<T extends Record> extends State<RecordForm<T>> {
               Padding(
                 padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
                 child: Column(
-                  children: [
-                    TextFormField(
-                        decoration: const InputDecoration(labelText: "Name"),
-                        validator: (value) => value?.isEmpty ?? true
-                            ? 'Please enter a name'
-                            : null,
-                        onChanged: (value) => record.title = value),
-                    LabelledCheckbox(label: 'Done', value: record.isDone),
-                    DateTimeTextField(
-                        labelText: 'Timestamp',
-                        value: record.timestamp,
-                        onChanged: (value) {
-                          record.timestamp = value!;
-                        },
-                        initialDate: DateTime.now()),
-                    NotesField(modelField: record.notes),
-                  ],
+                  children: _buildFields(),
                 ),
               ),
               Padding(
