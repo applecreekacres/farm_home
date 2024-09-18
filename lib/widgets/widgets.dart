bb405c71fc384d2df8192dbfe68f3a5f7120133c
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-18 11:17:07 -0500

    fix(widgets): exporting new classes

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,6 @@ export 'loading_view.dart';
 export 'drawer.dart';
 export 'notes.dart';
 export 'labeled_checkbox.dart';
+export 'record_form.dart';
+export 'resource_form.dart';
+export 'expandable_fab.dart';
