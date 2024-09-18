de690db3c679e8b26cb9ecc7884bc07ed004d14a
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-18 14:03:51 -0500

    fix(notes): cleaning up issues

--- lib/widgets/notes.dart
+++ lib/widgets/notes.dart
@@ -1,12 +1,10 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 
 class NotesField extends StatelessWidget {
-  String modelField;
+  final String modelField;
   final ValueChanged<String>? onChanged;
 
-  NotesField({required this.modelField, required this.onChanged, super.key});
+  const NotesField({required this.modelField, required this.onChanged, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +19,6 @@ class NotesField extends StatelessWidget {
         keyboardType: TextInputType.multiline,
         controller: TextEditingController(text: modelField),
         onChanged: (value) {
-          modelField = value;
           onChanged?.call(value);
         },
       )
