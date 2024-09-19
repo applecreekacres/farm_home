4f5760173b56ca7779714f330cbd324074222819
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-19 06:37:29 -0500

    fix: updates for null

--- lib/widgets/record_form.dart
+++ lib/widgets/record_form.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:farm_home/models/models.dart';
-import 'package:farm_home/widgets/datetime_text_field.dart';
 import 'package:farm_home/widgets/widgets.dart';
 
 class RecordForm<T extends Record> extends StatefulWidget {
   final T record;
   final List<Widget>? additionalFields;
+  final bool isNew;
 
-  const RecordForm({required this.record, this.additionalFields, super.key});
+  const RecordForm(
+      {required this.record,
+      required this.isNew,
+      this.additionalFields,
+      super.key});
 
   @override
   State<RecordForm> createState() => _RecordFormState<T>();
@@ -27,7 +31,7 @@ class _RecordFormState<T extends Record> extends State<RecordForm<T>> {
           validator: (value) =>
               value?.isEmpty ?? true ? 'Please enter a name' : null,
           onChanged: (value) => record.title = value),
-      LabelledCheckbox(label: 'Done', value: record.isDone),
+      LabelledCheckbox(label: 'Done', value: record.isDone!),
       DateTimeTextField(
           labelText: 'Timestamp',
           firstDate: DateTime(1970),
@@ -38,7 +42,7 @@ class _RecordFormState<T extends Record> extends State<RecordForm<T>> {
           },
           initialDate: DateTime.now()),
       NotesField(
-        modelField: record.notes,
+        modelField: record.notes!,
         onChanged: (value) {
           record.notes = value;
         },
@@ -61,7 +65,13 @@ class _RecordFormState<T extends Record> extends State<RecordForm<T>> {
             title: Text('New $recordName Record'),
             actions: [
               IconButton(
-                  onPressed: () => createItem<T>(record),
+                  onPressed: () {
+                    if (widget.isNew) {
+                      createItem<T>(record);
+                    } else {
+                      updateItem<T>(record);
+                    }
+                  },
                   icon: const Icon(Icons.save))
             ],
             bottom: const TabBar(
