e64e54140aefcaf3f165dbc866d0929480d4c5e9
Lucas Brendel <lucasbrendel@gmail.com>
2024-09-21 06:44:53 -0500

    fix: addng home navigation to drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:farm_home/pages/pages.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class FarmHomeDrawer extends StatelessWidget {
   const FarmHomeDrawer({super.key});
@@ -16,6 +17,14 @@ class FarmHomeDrawer extends StatelessWidget {
               BoxDecoration(color: Theme.of(context).colorScheme.primary),
           child: const Text("User"),
         ),
+        ListTile(
+          leading: const Icon(Symbols.house),
+          title: const Text("Home"),
+          onTap: () {
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const HomePage()));
+          },
+        )
       ],
     ));
   }
