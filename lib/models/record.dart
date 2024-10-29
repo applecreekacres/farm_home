b10229bdd4f06ab790cc1be29af699180f721e0b
Lucas Brendel <lucasbrendel@gmail.com>
2024-10-29 05:52:43 -0500

    fix(record): [wip] refactoring

--- lib/models/record.dart
+++ lib/models/record.dart
@@ -6,20 +6,31 @@ abstract class Record extends Item {
   String title = "";
   String notes = "";
   bool isDone = false;
-  List<Quantity> quantities = List<Quantity>.empty();
+  List<Quantity>? quantities = List<Quantity>.empty();
   DateTime? timestamp = DateTime.now();
-  List<Resource> resources = List<Resource>.empty();
-  List<String> tags = List<String>.empty();
+  List<String>? tags = List<String>.empty();
+  List<String>? resourceIds;
 
-  Record(
-      {this.title = "",
-      this.timestamp,
-      this.notes = "",
-      this.isDone = false,
-      this.quantities = const [],
-      this.resources = const [],
-      this.tags = const []})
-      : super() {
+  Future<List<Resource>?> get resources async {
+    List<Resource>? records;
+    if (resourceIds != null) {
+      for (var id in resourceIds!) {
+        records.add(await getItemById(id, (data) => ));
+      }
+    }
+
+    return records;
+  }
+
+  Record({
+    this.title = "",
+    this.timestamp,
+    this.notes = "",
+    this.isDone = false,
+    this.quantities,
+    this.tags,
+    this.resourceIds,
+  }) : super() {
     timestamp ??= DateTime.now();
   }
 
@@ -27,8 +38,8 @@ abstract class Record extends Item {
     title = data["title"];
     notes = data["notes"];
     isDone = data["isDone"];
+    resourceIds = List<String>.from(data["resourceIds"]);
     quantities = List<Quantity>.from(data["quantities"]);
-    resources = List<Resource>.from(data["resources"]);
     timestamp = DateTime.fromMillisecondsSinceEpoch(data["timestamp"]);
     tags = List<String>.from(data["tags"]);
   }
@@ -41,7 +52,7 @@ abstract class Record extends Item {
       "isDone": isDone,
       "quantities": quantities,
       "recordType": itemName(),
-      "resources": resources.map((res) => res.id).toList(),
+      "resourceIds": resourceIds,
       "timestamp": timestamp?.millisecondsSinceEpoch
     };
     map.addAll(super.toMap());
